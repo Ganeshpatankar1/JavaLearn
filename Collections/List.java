9eaf7059ed8d35effe12deed3a5973f8118b97ad
Ganeshpatankar1 <patankarg41@gmail.com>
2025-12-05 13:45:52 +0530

    Arraylist and LinkedList Program

--- Collections/List.java
+++ Collections/List.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class List {
     public static void main(String[] args) {
+        //Array List
         List<String> names = new ArrayList<>();
         names.add("Ganesh");
         names.add("Parth");
@@ -10,5 +12,12 @@ public class List {
 
         System.out.println(names);
         System.out.println("Pehala Name : " + names.get(0));    //Fast acess
+
+        //Linked List
+        List<String> cities = new LinkedList<>();
+        cities.add("Delhi");
+        cities.add("Mumbai");
+        cities.add(1,"Pune");   //Bich me add karna Fast hai
+        System.out.println(cities); //output [Delhi, Pune, Mumbai]
     }
 }
