d732a2c00767678823fd7999d86a15604cf9ee5d
Ganeshpatankar1 <patankarg41@gmail.com>
2025-12-07 09:43:00 +0530

    Add Overriding Program

--- BasicOfOOPs/OverridingFun.java
+++ BasicOfOOPs/OverridingFun.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
+class Bank{
+    public void RateOfInterest(){
+        System.out.println("Generally Rate Of Interest is 5%");
+    }
+}
+class PNB extends Bank{
+    @Override
+    public void RateOfInterest(){
+        System.out.println("Generally Rate Of Interest is 6.6%");
+    }
+}
+class HDFC extends Bank{
+    @Override
+    public void RateOfInterest() {
+        System.out.println("Generally Rate Of Interest is 5.6%");
+    }
+}
 public class OverridingFun {
+    public static void main(String[] args){
+        Bank obj = new Bank();
+        PNB obj1 = new PNB();
+        HDFC obj2 = new HDFC();
+        obj.RateOfInterest();
+        obj1.RateOfInterest();
+        obj2.RateOfInterest();
+    }
 }
