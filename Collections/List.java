975af121b5f78752b3d27f7227567bb6e93ad0ff
Ganeshpatankar1 <patankarg41@gmail.com>
2025-12-07 18:34:28 +0530

    List Programs

--- Collections/List.java
+++ Collections/List.java
@@ -3,10 +3,21 @@ import java.util.ArrayList;
 public class List {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
+        list.add(32);
+        list.add(43);
         list.add(23);
         list.add(42);
+        list.add(94);
+        list.add(42);
         System.out.println(list.get(2));
+        System.out.println(list.size());
+        for (int i=0; i<list.size(); i++){
+            System.out.println(list.get(i));
+        }
+        for (int x : list){
+            System.out.println(x);
+        }
+        System.out.println(list.contains(43));
+        System.out.println(list.contains(32));
     }
 }
