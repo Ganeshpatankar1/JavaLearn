1e48f24f06476a348645776e23fbe6ff734fee7c
Ganeshpatankar1 <patankarg41@gmail.com>
2025-12-07 18:29:36 +0530

    List Programs

--- Collections/List.java
+++ Collections/List.java
@@ -1,23 +1,12 @@
-//import java.util.ArrayList;
-//import java.util.LinkedList;
-//import java.util.List;
-//
-//public class List {
-//    public static void main(String[] args) {
-//        //Array List
-//        List<String> names = new ArrayList<>();
-//        names.add("Ganesh");
-//        names.add("Parth");
-//        names.add("Ganesh");    //Duplicate Allowed
-//
-//        System.out.println(names);
-//        System.out.println("Pehala Name : " + names.get(0));    //Fast acess
-//
-//        //Linked List
-//        List<String> cities = new LinkedList<>();
-//        cities.add("Delhi");
-//        cities.add("Mumbai");
-//        cities.add(1,"Pune");   //Bich me add karna Fast hai
-//        System.out.println(cities); //output [Delhi, Pune, Mumbai]
-//    }
-//}
+import java.util.ArrayList;
+
+public class List {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(23);
+        list.add(42);
+        System.out.println(list.get(2));
+    }
+}
