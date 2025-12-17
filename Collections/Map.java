e9e509b0f251a269c7f30a658eadc538259e53a8
Ganeshpatankar1 <patankarg41@gmail.com>
2025-12-17 12:22:57 +0530

    List Programs

--- Collections/Map.java
+++ Collections/Map.java
@@ -1,14 +1,14 @@
-//import java.util.HashMap;
-//
-//public class Map {
-//    public static void main(String[] args) {
-//        Map<String, Integer> studentMarks = new HashMap<>();
-//        studentMarks.put("Amit", 85);   //'Amit' is key, value 85
-//        studentMarks.put("Priya", 92);
-//        studentMarks.put("Ravi", 76);
-//        studentMarks.put("Priya", 95); // "Priya" ki value update ho jayegi
-//
-//        System.out.println(studentMarks); // Output: {Amit=85, Ravi=76, Priya=95} (order guaranteed nahi)
-//        System.out.println("Priya ke marks: " + studentMarks.get("Priya")); // Fast access using key)
-//    }
-//}
+import java.util.HashMap;
+
+public class Map {
+    public static void main(String[] args) {
+        Map<String, Integer> studentMarks = new HashMap<>();
+        studentMarks.put("Amit", 85);   //'Amit' is key, value 85
+        studentMarks.put("Priya", 92);
+        studentMarks.put("Ravi", 76);
+        studentMarks.put("Priya", 95); // "Priya" ki value update ho jayegi
+
+        System.out.println(studentMarks); // Output: {Amit=85, Ravi=76, Priya=95} (order guaranteed nahi)
+        System.out.println("Priya ke marks: " + studentMarks.get("Priya")); // Fast access using key)
+    }
+}
--- Collections/Set.java
+++ Collections/Set.java
@@ -1,29 +1,29 @@
-//import java.util.HashSet;
-//import java.util.LinkedHashSet;
-//
-//public class Set {
-//    public static void main(String[] args) {
-//        // HashSet: Order ki guarantee nahi
-//        Set<String> hashSet = new HashSet<>();
-//        hashSet.add("Banana");
-//        hashSet.add("apple");
-//        hashSet.add("Cherry");
-//        hashSet.add("apple");   //yeh add nahi hoga
-//        System.out.println("HashSet : " + hashSet);
-//
-//        // LinkedHashSet: Insertion order
-//        Set<String> linkedHashSet = new LinkedHashSet<>();
-//        linkedHashSet.add("Banana");
-//        linkedHashSet.add("Apple");
-//        linkedHashSet.add("Cherry");
-//        System.out.println("LinkedHashSet: " + linkedHashSet);
-//
-//        // TreeSet: Sorted order
-//        Set<String> treeSet = new TreeSet<>();
-//        treeSet.add("Banana");
-//        treeSet.add("Apple");
-//        treeSet.add("Cherry");
-//        System.out.println("TreeSet: " + treeSet);
-//
-//    }
-//}
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class Set {
+    public static void main(String[] args) {
+        // HashSet: Order ki guarantee nahi
+        Set<String> hashSet = new HashSet<>();
+        hashSet.add("Banana");
+        hashSet.add("apple");
+        hashSet.add("Cherry");
+        hashSet.add("apple");   //yeh add nahi hoga
+        System.out.println("HashSet : " + hashSet);
+
+        // LinkedHashSet: Insertion order
+        Set<String> linkedHashSet = new LinkedHashSet<>();
+        linkedHashSet.add("Banana");
+        linkedHashSet.add("Apple");
+        linkedHashSet.add("Cherry");
+        System.out.println("LinkedHashSet: " + linkedHashSet);
+
+        // TreeSet: Sorted order
+        Set<String> treeSet = new TreeSet<>();
+        treeSet.add("Banana");
+        treeSet.add("Apple");
+        treeSet.add("Cherry");
+        System.out.println("TreeSet: " + treeSet);
+
+    }
+}
