aa001cd138326196ca74d0f614988bcd59542aa5
Ganeshpatankar1 <patankarg41@gmail.com>
2025-12-23 18:49:21 +0530

    Practice Of Program

---
+++ Collections/ListAndSet/ListDemo.java
@@ -0,0 +1,15 @@
+package ListAndSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+    public static void main(String[] args) {
+        List l = new ArrayList();
+        l.add(1,30);
+        l.add(3,23);
+        l.add(42);
+        l.add(32);
+        System.out.println(l);
+    }
+}
--- Collections/ListAndSet/SetDemo.java
+++ Collections/ListAndSet/SetDemo.java
@@ -1,4 +1,15 @@
 package ListAndSet;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SetDemo {
+    public static void main(String[] args) {
+        Set s = new HashSet();
+        s.add(10);
+        s.add(20);
+        s.add(30);
+        s.add(40);
+        System.out.println(s);
+    }
 }
