061340ee00ee18444f7ad3b42f8d9c7216780ce5
Ganeshpatankar1 <patankarg41@gmail.com>
2025-12-23 19:09:39 +0530

    Practice Of Set

--- Collections/ListEx/ArrayListEx.java
+++ Collections/ListEx/ArrayListEx.java
@@ -1,5 +1,7 @@
 package ListEx;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 
 public class ArrayListEx {
@@ -12,17 +14,23 @@ public class ArrayListEx {
         name.add(83);
         name.add(48);
         name.add(83);
-
+        System.out.println("\n");
         for (Integer i : name){
             System.out.println("Number He jo ki = " + i);
         }
+        System.out.println("\n");
 
         Iterator<Integer> it = name.iterator();
         while (it.hasNext()){
             System.out.println(it.next());
         }
+        System.out.println("\n");
+
         if (name.contains(83)){
             System.out.println("Are Wah 83 to list me he Ok!");
         }
+        System.out.println("\n");
+        Collections.sort(name);
+        System.out.println(name);
     }
 }
