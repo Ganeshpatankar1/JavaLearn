ba55db9d68ea46c231cee84f2ec7a87dfaafa664
Ganeshpatankar1 <patankarg41@gmail.com>
2025-12-23 18:36:29 +0530

    Practice Of Set

---
+++ Collections/ListAndSet/SetDemo.java
@@ -0,0 +1,4 @@
+package ListAndSet;
+
+public class SetDemo {
+}
---
+++ Collections/ListEx/ListEx.java
@@ -0,0 +1,31 @@
+package ListEx;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class ListEx {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add(21);
+        al.add(43);
+        al.add(42);
+        al.add(53);
+        System.out.println(al);
+
+        HashSet hset = new HashSet();
+        hset.add(43);
+        hset.add(43);
+        hset.add(65);
+        hset.add(94);
+        hset.add(42);
+        System.out.println(hset);
+
+        HashMap hmap = new HashMap();
+        hmap.put(1,"Ganesh");
+        hmap.put(2,"Sanket");
+        hmap.put(3,"Mangesh");
+        hmap.put(4,"Sangram");
+        System.out.println(hmap);
+    }
+}
