85e86396db7bcea7a8b18bd6cc619c36f4c811c6
Ganeshpatankar1 <patankarg41@gmail.com>
2025-12-24 18:01:19 +0530

    Programs

--- Collections/ListEx/MilkShop.java
+++ Collections/ListEx/MilkShop.java
@@ -4,16 +4,16 @@ import java.util.HashMap;
 
 public class MilkShop {
     public static void main(String[] args) {
-        HashMap<Integer, String> map = new HashMap<Integer, String>();
-        map.put(100, "Milk");
-        map.put(200, "Mango");
-        map.put(10, "Maggie");
-        map.put(30, "Biscuit");
-        map.put(50, "Sugar");
+        HashMap<String, Integer> map = new HashMap<String,Integer>();
+        map.put("Milk", 100);
+        map.put("Mango",312);
+        map.put("Maggie",212);
+        map.put("Biscuit",21);
+        map.put("Sugar",231);
 
-        String name = map.get(100);
-        System.out.println("100 Price = " + name);
-        map.remove(10);
+        String price = String.valueOf(map.get("Milk"));
+        System.out.println("Milk Price Of = " + price + "rs");
+        map.remove("Maggie");
         System.out.println(map);
 
     }
