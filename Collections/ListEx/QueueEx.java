08f26103819b90984df6620a8f0dfbdabc4b299a
Ganeshpatankar1 <patankarg41@gmail.com>
2025-12-25 19:15:35 +0530

    Collections Program

--- Collections/ListEx/QueueEx.java
+++ Collections/ListEx/QueueEx.java
@@ -1,4 +1,21 @@
 package ListEx;
 
+import java.util.PriorityQueue;
+
 public class QueueEx {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+        queue.add(32);
+        queue.add(12);
+        queue.add(38);
+        queue.add(23);
+        queue.add(42);
+        queue.add(12);
+        System.out.println("Line atta Ashi Ahe " + queue +"\n");
+        System.out.println("Pahila Kon ahe " + queue.peek());
+        int first = queue.poll();
+        System.out.println("Ha Number Kam Kela ani Gela " + first);
+        System.out.println("Atta Pahila Kon Ahe " + queue.peek());
+
+    }
 }
