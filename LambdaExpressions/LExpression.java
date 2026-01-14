fb22bbc9a6a1b7d1651466c2e5e883bde85825e9
Ganeshpatankar1 <patankarg41@gmail.com>
2026-01-14 19:09:02 +0530

    Lambda List Programs Practice List

--- LambdaExpressions/LExpression.java
+++ LambdaExpressions/LExpression.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
-public class LExpression {
-}
+//Runnable r1 = new Runnable() {
+//    public void run(){
+//        System.out.println("Old Java is Running...");
+//    }
+//};
+//
+////naya java 8 tarika (Lamba)
+////() ka matalab method argument, -> ka matalab body
+//Runnable r2 = () -> System.out.println("Java 8 is Running")
--- LambdaExpressions/LambdaStream.java
+++ LambdaExpressions/LambdaStream.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+//Ek Function Interface Banate he
+@FunctionalInterface
+interface Greeting{
+    void sayHello(String name);
+}
 
 public class LambdaStream {
+    public static void main(String[] args) {
+        //Lambda Expression
+        //bina Class Banaye Method Define Kar diya
+        Greeting g = (name) -> System.out.println("Hello " + name + ", Welcome to Java 8");
+        g.sayHello("Rahul");
+    }
 }
